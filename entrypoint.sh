cb1d276caab814688ff3878ac47a7234eb508c5b
akademia-biblio <chrudzimska@snappzone.com>
2021-06-27 01:36:48 +0200

    main

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 
 lftp $INPUT_HOST -u $INPUT_USER,$INPUT_PASSWORD -e "set ftp:ssl-force $INPUT_FORCESSL; set ssl:verify-certificate false;
 mirror $INPUT_OPTIONS --reverse --continue --only-newer --dereference -x ^\.git/$ $INPUT_LOCALDIR $INPUT_REMOTEDIR; 
-mv public_html/sveltekit public_html/sveltekit_old; 
-mv public_html/sveltekit_load public_html/sveltekit; 
-mv public_html/sveltekit_old public_html/sveltekit_load; 
+mv public_html/demo-5 public_html/demo-5_old; 
+mv public_html/demo-5_load public_html/demo-5; 
+mv public_html/demo-5_old public_html/demo-5_load; 
 quit"
 
 # rm -r public_html/sveltekit_old;
